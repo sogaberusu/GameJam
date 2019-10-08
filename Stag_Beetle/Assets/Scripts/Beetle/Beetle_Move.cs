724883ff50662f1e42d152ae6bbce4b4ee75cbbf
sogabesatoshi <kbc17b14@stu.kawahara.ac.jp>
2019-10-08 17:50:43 +0900

    10月8日分

---
+++ Stag_Beetle/Assets/Scripts/Beetle/Beetle_Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Beetle_Move : MonoBehaviour
+{
+    Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animation>();
+
+        anim.Play("Dynastid beetle_male_idle2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //クワガタとの距離が近かったらに変更予定
+        if (Input.GetKey("joystick button 1"))
+        {
+            anim.Play("Dynastid beetle_male_attack2");
+        }
+        else if (Input.GetKey("joystick button 0"))
+        {
+            transform.Translate(0.0f, 0.0f, 0.05f);
+            anim.Play("Dynastid beetle_male_walk");
+        }
+        else if (Input.GetKey("joystick button 2"))
+        {
+            transform.Translate(0.0f, 0.0f, -0.05f);
+            anim.Play("Dynastid beetle_male_walk");
+        }
+        else
+        {
+            anim.Play("Dynastid beetle_male_idle2");
+        }
+    }
+}
