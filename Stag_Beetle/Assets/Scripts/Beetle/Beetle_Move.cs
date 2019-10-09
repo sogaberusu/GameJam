d3d8f57dbee0b02cb95b0720ecac6af785169723
sogabesatoshi <kbc17b14@stu.kawahara.ac.jp>
2019-10-09 15:20:03 +0900

    カブトムシ

--- Stag_Beetle/Assets/Scripts/Beetle/Beetle_Move.cs
+++ Stag_Beetle/Assets/Scripts/Beetle/Beetle_Move.cs
@@ -4,36 +4,60 @@ using UnityEngine;
 public class Beetle_Move : MonoBehaviour
 {
     Animation anim;
-
+    Transform player;
+    enum BeetleState
+    {
+        idle,
+        advance,
+        recession,
+        attack
+    }
+    BeetleState beetleState = BeetleState.advance;
     // Start is called before the first frame update
     void Start()
     {
         anim = this.gameObject.GetComponent<Animation>();
 
         anim.Play("Dynastid beetle_male_idle2");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //クワガタとの距離が近かったらに変更予定
-        if (Input.GetKey("joystick button 1"))
+        player = GameObject.Find("Dorcus_titanus_male").transform;
+        Vector3 playerPos = player.transform.position;
+        Vector3 beetlePos = this.transform.position;
+        float dis = Vector3.Distance(playerPos, beetlePos);
+        Debug.Log("Distance:" +dis);
+
+        //クワガタとの距離が近かったら攻撃
+        if (dis < 30.0f )
         {
-            anim.Play("Dynastid beetle_male_attack2");
+            beetleState = BeetleState.attack;
         }
-        else if (Input.GetKey("joystick button 0"))
-        {
-            transform.Translate(0.0f, 0.0f, 0.05f);
-            anim.Play("Dynastid beetle_male_walk");
-        }
-        else if (Input.GetKey("joystick button 2"))
-        {
-            transform.Translate(0.0f, 0.0f, -0.05f);
-            anim.Play("Dynastid beetle_male_walk");
-        }
-        else
+
+
+
+        if(beetleState == BeetleState.idle)
         {
             anim.Play("Dynastid beetle_male_idle2");
+        }
+        if (beetleState == BeetleState.advance)
+        {
+            transform.position += transform.forward * 0.05f;
+            anim.Play("Dynastid beetle_male_walk");
+        }
+        if (beetleState == BeetleState.recession)
+        {
+            transform.position += transform.forward * -0.05f;
+            anim.Play("Dynastid beetle_male_walk");
+        }
+        if (beetleState == BeetleState.attack)
+        {
+            anim.Play("Dynastid beetle_male_attack2");
+
+            beetleState = BeetleState.recession;
         }
     }
 }
